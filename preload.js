7dd3157ad3681e687a3194852a344c1cc74506a1
kujyonatsume <adam200134@gmail.com>
2025-03-19 09:53:12 +0800

    萬用IPC

--- preload.js
+++ preload.js
@@ -1,10 +1,8 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('ipcContext', {
-    send: (channel, ...data) => {
-        ipcRenderer.send(channel, ...data);
-    },
-    on: (channel, callback) => {
-        ipcRenderer.on(channel, (event, ...args) => callback(...args));
-    }
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
+//通过contextBridge 向渲染进程暴露一个全局的window.electronAPI
+electron_1.contextBridge.exposeInMainWorld('ipc', {
+    send: (...args) => electron_1.ipcRenderer.send('send', ...args),
+    invoke: (...args) => electron_1.ipcRenderer.invoke('invoke', ...args)
 });
---
+++ preload.ts
@@ -0,0 +1,13 @@
+import { contextBridge, ipcRenderer } from "electron";
+
+declare global {
+  interface Window {
+    ipc: any;
+  }
+}
+
+//通过contextBridge 向渲染进程暴露一个全局的window.electronAPI
+contextBridge.exposeInMainWorld('ipc', {
+  send: (...args: any) => ipcRenderer.send('send', ...args),
+  invoke: (...args: any) => ipcRenderer.invoke('invoke', ...args)
+})
